9e59b0023b997e8102150a2fd60099b0c38b1939
Vanboii <144983255+Vanboii@users.noreply.github.com>
2025-04-01 22:14:22 +0800

    test1 model happy news 87% accuracy
    datasplitter fn split into 2 , one for train and test, one for k-folds

--- datasetSplitter.py
+++ datasetSplitter.py
@@ -10,7 +10,6 @@ def datasetSplitterStratKFold(path:str=".\data\processed_data.csv",
                               randomState:int = 42):
     """
     - Splits the dataset into trainingSets & testSets
-    - Applies Stratified K Fold onto the training set
 
     Returns:
         [ (train_x, valid_x, train_y, valid_y) ] , X_test, y_test
@@ -28,22 +27,36 @@ def datasetSplitterStratKFold(path:str=".\data\processed_data.csv",
     y = pd.DataFrame(data["DR"])
 
     X_kfold, X_test, y_kfold, y_test = train_test_split(X, y, test_size=testSize, random_state=randomState,stratify=y)
+    return X_kfold, X_test, y_kfold, y_test
 
+def datasetFoldMaker(x_kFold:pd.DataFrame,
+                     y_kFold:pd.DataFrame,
+                     folds:int = 5,
+                     normalize:bool=True,
+                     randomState:int=42):
+    """
+    - Applies Stratified K Fold onto the training set
+    - Normalizes X for each Fold
+
+    Returns:
+        [ (train_x, valid_x, train_y, valid_y) ]
+
+        """
     if folds and folds > 1:
         kF = StratifiedKFold(n_splits=folds,shuffle=True, random_state=randomState)
 
-        kFold = []
+        kFolds = []
 
-        for train_idx, test_idx in kF.split(X_kfold,y_kfold):
-            # print(f"Fold: {fold}", train_idx, test_idx)
-            # print(f"Frame: {fold}", X_kfold.shape, y_kfold.shape)
-            train_x, train_y = X_kfold.iloc[train_idx], y_kfold.iloc[train_idx]
-            test_x, test_y = X_kfold.iloc[test_idx], y_kfold.iloc[test_idx]
-            # print(f"Fold: {fold}")
-            # print(f"Train: {train_x.shape}, {train_y.shape}")
-            # print(f"Test: {test_x.shape}, {test_y.shape}")
-            kFold.append((train_x, test_x, train_y, test_y))
+        for train_idx, test_idx in kF.split(x_kFold,y_kFold):
 
-        return kFold, X_test, y_test
-    else:
-        return X_kfold, X_test, y_kfold, y_test
+            train_x, train_y = x_kFold.iloc[train_idx], y_kFold.iloc[train_idx]
+            test_x, test_y = x_kFold.iloc[test_idx], y_kFold.iloc[test_idx]
+
+            if normalize:
+                train_x = (train_x - train_x.min()) / (train_x.max() - train_x.min())
+                test_x = (test_x - test_x.min()) / (test_x.max() - test_x.min())
+            
+            kFolds.append((train_x, test_x, train_y, test_y))
+
+        return kFolds
+    return None
